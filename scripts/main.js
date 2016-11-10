7a3d4fbc46c1b76ba65208297a1769b373484229
Marian Powell <marianpowell@gmail.com>
2016-11-10 16:28:00 -0500

    buttons open sections

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,30 @@
 $( document ).ready(function() {
-  $('.section').find('li').hide();
-    $('.section').on('click',function(){
-      $(this).find('li').slideUp();
-      $(this).find('li').toggle();
+
+    $('.section').on('click',function(event){
+      $('.section').hide();
+      var target = $(event.target);
+      $(target).toggleClass('open').show();
+      if($(target).hasClass('open')){
+        var context = {title: "My New Post", address: "github.org", body: "This is my first post!"};
+        var html = makeTemplate(context,'#projects-template');
+        $(target).children('.content').append(html);
+        $('.section').css('width','100%');
+
+     } else {
+         $(target).children('.content').empty();
+         $('.section').css('width','32.25%');
+         $('.section').show();
+     }
     });
 
+function makeTemplate(context,target){
+  var source   = $(target).html();
+  var template = Handlebars.compile(source);
+  return template(context);
 
+}
 
-
+// target.children().toggle();
 
 
 
